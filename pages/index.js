ba4b83ec1b6eccfbdff547ae10358a0ec5344c24
Bradley Genao <bjgenao@gmail.com>
2021-07-16 13:58:44 -0700

    Smmall bug fix to show something if no post are loaded

--- pages/index.js
+++ pages/index.js
@@ -45,31 +45,32 @@ function Index({ user, postsData, errorLoading }) {
         }
     };
 
-    if (posts.length === 0 || errorLoading) return <NoPosts />;
-
     return (
         <>
             {showToastr && <PostDeleteToastr />}
             <Segment>
                 <CreatePost user={user} setPosts={setPosts} />
-
-                <InfiniteScroll
-                    hasMore={hasMore}
-                    next={fetchDataOnScroll}
-                    loader={<PlaceHolderPosts />}
-                    endMessage={<EndMessage />}
-                    dataLength={posts.length}
-                >
-                    {posts.map((post) => (
-                        <CardPost
-                            key={post._id}
-                            post={post}
-                            user={user}
-                            setPosts={setPosts}
-                            setShowToastr={setShowToastr}
-                        />
-                    ))}
-                </InfiniteScroll>
+                {posts.length === 0 || errorLoading ? (
+                    <NoPosts />
+                ) : (
+                    <InfiniteScroll
+                        hasMore={hasMore}
+                        next={fetchDataOnScroll}
+                        loader={<PlaceHolderPosts />}
+                        endMessage={<EndMessage />}
+                        dataLength={posts.length}
+                    >
+                        {posts.map((post) => (
+                            <CardPost
+                                key={post._id}
+                                post={post}
+                                user={user}
+                                setPosts={setPosts}
+                                setShowToastr={setShowToastr}
+                            />
+                        ))}
+                    </InfiniteScroll>
+                )}
             </Segment>
         </>
     );
