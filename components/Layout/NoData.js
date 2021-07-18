1fe8f8235c2a87fd62720186b7a3a6a7726cb794
Bradley Genao <bjgenao@gmail.com>
2021-07-18 09:51:07 -0700

    Apit routes to get user profile info and posts of user, api to update profile, api to update password and message mpupup

--- components/Layout/NoData.js
+++ components/Layout/NoData.js
@@ -1,42 +1,60 @@
-import { Message, Button } from "semantic-ui-react";
+import { Message, Button } from 'semantic-ui-react';
 
 export const NoProfilePosts = () => (
-  <>
-    <Message info icon="meh" header="Sorry" content="User has not posted anything yet!" />
-    <Button icon="long arrow alternate left" content="Go Back" as="a" href="/" />
-  </>
+    <>
+        <Message
+            info
+            icon="meh"
+            header="Sorry"
+            content="User has not posted anything yet!"
+        />
+        <Button
+            icon="long arrow alternate left"
+            content="Go Back"
+            as="a"
+            href="/"
+        />
+    </>
 );
 
 export const NoFollowData = ({ followersComponent, followingComponent }) => (
-  <>
-    {followersComponent && (
-      <Message icon="user outline" info content="User does not have followers" />
-    )}
+    <>
+        {followersComponent && (
+            <Message
+                icon="user outline"
+                info
+                content="User does not have followers"
+            />
+        )}
 
-    {followingComponent && (
-      <Message icon="user outline" info content="User does not follow any users" />
-    )}
-  </>
+        {followingComponent && (
+            <Message
+                icon="user outline"
+                info
+                content="User does not follow any users"
+            />
+        )}
+    </>
 );
 
 export const NoMessages = () => (
-  <Message
-    info
-    icon="telegram plane"
-    header="Sorry"
-    content="You have not messaged anyone yet.Search above to message someone!"
-  />
+    <Message
+        info
+        icon="telegram plane"
+        header="Sorry"
+        content="You have not messaged anyone yet.Search above to message someone!"
+    />
 );
 
 export const NoPosts = () => (
-  <Message
-    info
-    icon="meh"
-    header="Hey!"
-    content="No Posts. Make sure you have followed someone."
-  />
+    <Message
+        info
+        icon="meh"
+        header="Hey!"
+        content="No Posts. Make sure you have followed someone."
+    />
 );
 
 export const NoProfile = () => (
-  <Message info icon="meh" header="Hey!" content="No Profile Found." />
+    <Message info icon="meh" header="Hey!" content="No Profile Found." />
 );
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ nextApp.prepare().then(() => {
     app.use('/api/auth', require('./api/auth'));
     app.use('/api/search', require('./api/search'));
     app.use('/api/posts', require('./api/posts'));
+    app.use('/api/profile', require('./api/profile'))
 
     app.all('*', (req, res) => handle(req, res));
 
