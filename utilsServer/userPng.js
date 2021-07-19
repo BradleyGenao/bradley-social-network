a29435d3a3d080d4e5f438bb47da8e76e6dd8984
Bradley Genao <bjgenao@gmail.com>
2021-07-19 11:30:29 -0700

    Added profile page

--- utilsServer/userPng.js
+++ utilsServer/userPng.js
@@ -1,2 +1,2 @@
 const userPng =
-  "https://res.cloudinary.com/indersingh/image/upload/v1593464618/App/user_mklcpl.png";
+    'https://res.cloudinary.com/bjgenao/image/upload/v1626385276/default%20/user_default.png';
