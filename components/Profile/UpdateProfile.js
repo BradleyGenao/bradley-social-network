7d3097989f8943bf18f61faeb0899aa5cbe39543
Bradley Genao <bjgenao@gmail.com>
2021-07-19 08:52:51 -0700

    Added the update profile componenet and created a root user

--- components/Profile/UpdateProfile.js
+++ components/Profile/UpdateProfile.js
@@ -90,7 +90,7 @@ function UpdateProfile({ Profile }) {
           color="blue"
           icon="pencil alternate"
           disabled={profile.bio === "" || loading}
-          content="Submit"
+          content="Save"
           type="submit"
         />
       </Form>
