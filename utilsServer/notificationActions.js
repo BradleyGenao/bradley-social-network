c855560526bf646b8292a3f06906e584a7a9e94d
Bradley Genao <bjgenao@gmail.com>
2021-07-19 12:31:10 -0700

    addd and remove a comment notification using enterprise level for remove

--- utilsServer/notificationActions.js
+++ utilsServer/notificationActions.js
@@ -99,22 +99,21 @@ const removeCommentNotification = async (
     userToNotifyId
 ) => {
     try {
-        const user = await NotificationModel.findOne({ user: userToNotifyId });
-
-        const notificationToRemove = await user.notifications.find(
-            (notification) =>
-                notification.type === 'newComment' &&
-                notification.user.toString() === userId &&
-                notification.post.toString() === postId &&
-                notification.commentId === commentId
+        await NotificationModel.findOneAndUpdate(
+            { user: userToNotifyId },
+            {
+                $pull: {
+                    notifications: {
+                        type: 'newComment',
+                        user: userId,
+                        post: postId,
+                        commentId: commentId,
+                    },
+                },
+            }
         );
 
-        const indexOf = await user.notifications
-            .map((notification) => notification._id.toString())
-            .indexOf(notificationToRemove._id.toString());
-
-        await user.notifications.splice(indexOf, 1);
-        await user.save();
+        return;
     } catch (error) {
         console.error(error);
     }
