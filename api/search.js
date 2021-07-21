12e644947c28c641c7278b0694ccbbc28e6ad40b
Bradley Genao <bjgenao@gmail.com>
2021-07-21 13:48:42 -0700

    Updated seach api so if the user cant serach for themselves

--- api/search.js
+++ api/search.js
@@ -6,14 +6,23 @@ const UserModel = require('../models/UserModel');
 router.get('/:searchText', authMiddleware, async (req, res) => {
     try {
         const { searchText } = req.params;
+        const { userId } = req;
 
         if (searchText.length === 0) return;
 
+        let userPattern = new RegExp(`^${searchText}`);
+
         const results = await UserModel.find({
-            name: { $regex: searchText, $options: 'i' },
+            name: { $regex: userPattern, $options: 'i' },
         });
 
-        return res.status(200).json(results);
+        const resultsToBeSent =
+            results.length > 0 &&
+            results.filter((result) => result._id.toString() !== userId);
+
+        return res
+            .status(200)
+            .json(resultsToBeSent.length > 0 ? resultsToBeSent : results);
     } catch (error) {
         console.error(error);
         return res.status(500).send(`Server error`);
