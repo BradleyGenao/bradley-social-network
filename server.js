acef7b759f7eb41be758c3e41ab070a7ac54177b
Bradley Genao <bjgenao@gmail.com>
2021-07-21 14:51:30 -0700

    Displaying online users, fetching messages of a chat

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const io = require('socket.io')(server);
 const next = require('next');
 const dev = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev });
@@ -10,13 +11,94 @@ const connectDb = require('./utilsServer/connectDb');
 connectDb();
 app.use(express.json());
 const PORT = process.env.PORT || 3000;
+const {
+    addUser,
+    removeUser,
+    findConnectedUser,
+} = require('./utilsServer/roomActions');
+const {
+    loadMessages,
+    sendMsg,
+    setMsgToUnread,
+    deleteMsg,
+} = require('./utilsServer/messageActions');
+
+io.on('connection', (socket) => {
+    socket.on('join', async ({ userId }) => {
+        const users = await addUser(userId, socket.id);
+        console.log(users);
+
+        setInterval(() => {
+            socket.emit('connectedUsers', {
+                users: users.filter((user) => user.userId !== userId),
+            });
+        }, 10000);
+    });
+
+    socket.on('loadMessages', async ({ userId, messagesWith }) => {
+        const { chat, error } = await loadMessages(userId, messagesWith);
+
+        !error
+            ? socket.emit('messagesLoaded', { chat })
+            : socket.emit('noChatFound');
+    });
+
+    socket.on('sendNewMsg', async ({ userId, msgSendToUserId, msg }) => {
+        const { newMsg, error } = await sendMsg(userId, msgSendToUserId, msg);
+        const receiverSocket = findConnectedUser(msgSendToUserId);
+
+        if (receiverSocket) {
+            // WHEN YOU WANT TO SEND MESSAGE TO A PARTICULAR SOCKET
+            io.to(receiverSocket.socketId).emit('newMsgReceived', { newMsg });
+        }
+        //
+        else {
+            await setMsgToUnread(msgSendToUserId);
+        }
+
+        !error && socket.emit('msgSent', { newMsg });
+    });
+
+    socket.on('deleteMsg', async ({ userId, messagesWith, messageId }) => {
+        const { success } = await deleteMsg(userId, messagesWith, messageId);
+
+        if (success) socket.emit('msgDeleted');
+    });
+
+    socket.on(
+        'sendMsgFromNotification',
+        async ({ userId, msgSendToUserId, msg }) => {
+            const { newMsg, error } = await sendMsg(
+                userId,
+                msgSendToUserId,
+                msg
+            );
+            const receiverSocket = findConnectedUser(msgSendToUserId);
+
+            if (receiverSocket) {
+                // WHEN YOU WANT TO SEND MESSAGE TO A PARTICULAR SOCKET
+                io.to(receiverSocket.socketId).emit('newMsgReceived', {
+                    newMsg,
+                });
+            }
+            //
+            else {
+                await setMsgToUnread(msgSendToUserId);
+            }
+
+            !error && socket.emit('msgSentFromNotification');
+        }
+    );
+
+    socket.on('disjoin', () => removeUser(socket.id));
+});
 
 nextApp.prepare().then(() => {
     app.use('/api/signup', require('./api/signup'));
     app.use('/api/auth', require('./api/auth'));
     app.use('/api/search', require('./api/search'));
     app.use('/api/posts', require('./api/posts'));
-    app.use('/api/profile', require('./api/profile'))
+    app.use('/api/profile', require('./api/profile'));
     app.use('/api/notifications', require('./api/notifications'));
     app.use('/api/chats', require('./api/chats'));
 
@@ -24,6 +106,6 @@ nextApp.prepare().then(() => {
 
     server.listen(PORT, (err) => {
         if (err) throw err;
-        console.log(`Express server running on ${PORT}`);
+        console.log('Express server running');
     });
 });
--- utilsServer/messageActions.js
+++ utilsServer/messageActions.js
@@ -1,122 +1,131 @@
-const ChatModel = require("../models/ChatModel");
-const UserModel = require("../models/UserModel");
+const ChatModel = require('../models/ChatModel');
+const UserModel = require('../models/UserModel');
 
 const loadMessages = async (userId, messagesWith) => {
-  try {
-    const user = await ChatModel.findOne({ user: userId }).populate("chats.messagesWith");
+    try {
+        const user = await ChatModel.findOne({ user: userId }).populate(
+            'chats.messagesWith'
+        );
 
-    const chat = user.chats.find(
-      chat => chat.messagesWith._id.toString() === messagesWith
-    );
+        const chat = user.chats.find(
+            (chat) => chat.messagesWith._id.toString() === messagesWith
+        );
 
-    if (!chat) {
-      return { error: "No chat found" };
+        if (!chat) {
+            return { error: 'No chat found' };
+        }
+
+        return { chat };
+    } catch (error) {
+        console.log(error);
+        return { error };
     }
-
-    return { chat };
-  } catch (error) {
-    console.log(error);
-    return { error };
-  }
 };
 
 const sendMsg = async (userId, msgSendToUserId, msg) => {
-  try {
-    // LOGGED IN USER (SENDER)
-    const user = await ChatModel.findOne({ user: userId });
+    try {
+        // LOGGED IN USER (SENDER)
+        const user = await ChatModel.findOne({ user: userId });
 
-    // RECEIVER
-    const msgSendToUser = await ChatModel.findOne({ user: msgSendToUserId });
+        // RECEIVER
+        const msgSendToUser = await ChatModel.findOne({
+            user: msgSendToUserId,
+        });
 
-    const newMsg = {
-      sender: userId,
-      receiver: msgSendToUserId,
-      msg,
-      date: Date.now()
-    };
+        const newMsg = {
+            sender: userId,
+            receiver: msgSendToUserId,
+            msg,
+            date: Date.now(),
+        };
 
-    const previousChat = user.chats.find(
-      chat => chat.messagesWith.toString() === msgSendToUserId
-    );
+        const previousChat = user.chats.find(
+            (chat) => chat.messagesWith.toString() === msgSendToUserId
+        );
 
-    if (previousChat) {
-      previousChat.messages.push(newMsg);
-      await user.save();
+        if (previousChat) {
+            previousChat.messages.push(newMsg);
+            await user.save();
+        }
+        //
+        else {
+            const newChat = {
+                messagesWith: msgSendToUserId,
+                messages: [newMsg],
+            };
+            user.chats.unshift(newChat);
+            await user.save();
+        }
+
+        const previousChatForReceiver = msgSendToUser.chats.find(
+            (chat) => chat.messagesWith.toString() === userId
+        );
+
+        if (previousChatForReceiver) {
+            previousChatForReceiver.messages.push(newMsg);
+            await msgSendToUser.save();
+        }
+        //
+        else {
+            const newChat = { messagesWith: userId, messages: [newMsg] };
+            msgSendToUser.chats.unshift(newChat);
+            await msgSendToUser.save();
+        }
+
+        return { newMsg };
+    } catch (error) {
+        console.error(error);
+        return { error };
     }
-    //
-    else {
-      const newChat = { messagesWith: msgSendToUserId, messages: [newMsg] };
-      user.chats.unshift(newChat);
-      await user.save();
-    }
-
-    const previousChatForReceiver = msgSendToUser.chats.find(
-      chat => chat.messagesWith.toString() === userId
-    );
-
-    if (previousChatForReceiver) {
-      previousChatForReceiver.messages.push(newMsg);
-      await msgSendToUser.save();
-    }
-    //
-    else {
-      const newChat = { messagesWith: userId, messages: [newMsg] };
-      msgSendToUser.chats.unshift(newChat);
-      await msgSendToUser.save();
-    }
-
-    return { newMsg };
-  } catch (error) {
-    console.error(error);
-    return { error };
-  }
 };
 
-const setMsgToUnread = async userId => {
-  try {
-    const user = await UserModel.findById(userId);
+const setMsgToUnread = async (userId) => {
+    try {
+        const user = await UserModel.findById(userId);
 
-    if (!user.unreadMessage) {
-      user.unreadMessage = true;
-      await user.save();
+        if (!user.unreadMessage) {
+            user.unreadMessage = true;
+            await user.save();
+        }
+
+        return;
+    } catch (error) {
+        console.error(error);
     }
-
-    return;
-  } catch (error) {
-    console.error(error);
-  }
 };
 
 const deleteMsg = async (userId, messagesWith, messageId) => {
-  try {
-    const user = await ChatModel.findOne({ user: userId });
+    try {
+        const user = await ChatModel.findOne({ user: userId });
 
-    const chat = user.chats.find(chat => chat.messagesWith.toString() === messagesWith);
+        const chat = user.chats.find(
+            (chat) => chat.messagesWith.toString() === messagesWith
+        );
 
-    if (!chat) return;
+        if (!chat) return;
 
-    const messageToDelete = chat.messages.find(
-      message => message._id.toString() === messageId
-    );
+        const messageToDelete = chat.messages.find(
+            (message) => message._id.toString() === messageId
+        );
 
-    if (!messageToDelete) return;
+        if (!messageToDelete) return;
 
-    if (messageToDelete.sender.toString() !== userId) {
-      return;
+        if (messageToDelete.sender.toString() !== userId) {
+            return;
+        }
+
+        const indexOf = chat.messages
+            .map((message) => message._id.toString())
+            .indexOf(messageToDelete._id.toString());
+
+        await chat.messages.splice(indexOf, 1);
+
+        await user.save();
+
+        return { success: true };
+    } catch (error) {
+        console.log(error);
     }
-
-    const indexOf = chat.messages
-      .map(message => message._id.toString())
-      .indexOf(messageToDelete._id.toString());
-
-    await chat.messages.splice(indexOf, 1);
-
-    await user.save();
-
-    return { success: true };
-  } catch (error) {
-    console.log(error);
-  }
 };
 
 module.exports = { loadMessages, sendMsg, setMsgToUnread, deleteMsg };
