549383492c2f6d720d26721b6ee049197242f7a3
Bradley Genao <bjgenao@gmail.com>
2021-07-23 10:55:16 -0700

    rtying to get it to deploy on vercel

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
     process.env.NODE_ENV !== 'production'
         ? 'http://localhost:3000'
-        : 'https://social-network-alpha.vercel.app/login';
+        : 'https://social-network-alpha.vercel.app';
 
 module.exports = baseUrl;
