207edcfdf9f829de6f86df4dc7b30a3940c3fe97
Bradley Genao <bjgenao@gmail.com>
2021-07-23 11:01:24 -0700

    ...

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
     process.env.NODE_ENV !== 'production'
         ? 'http://localhost:3000'
-        : 'https://social-network-alpha.vercel.app';
+        : 'https://social-network-sigma.vercel.app';
 
 module.exports = baseUrl;
