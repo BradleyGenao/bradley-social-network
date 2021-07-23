70954dd7a1c9560cb860632f45b905dfccbd8f66
Bradley Genao <bjgenao@gmail.com>
2021-07-23 10:45:31 -0700

    fixed base url

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
     process.env.NODE_ENV !== 'production'
         ? 'http://localhost:3000'
-        : 'https://social-network.vercel.app';
+        : 'https://social-network-alpha.vercel.app/login';
 
 module.exports = baseUrl;
